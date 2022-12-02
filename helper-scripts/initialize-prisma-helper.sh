aee96be88a91d6527183ea5b4c0f0e9ff0bcda78
Gil3co <68588828+Gil3co@users.noreply.github.com>
2022-12-02 10:45:36 +0200

    Update initialize-prisma-helper.sh

--- helper-scripts/initialize-prisma-helper.sh
+++ helper-scripts/initialize-prisma-helper.sh
@@ -7,7 +7,6 @@ npm install @prisma/client
 cp ../schema.prisma ./prisma/schema.prisma
 cp ../.env .
 
-
 if [[ $1 == "isGql" ]]
 then
   cp ../gql-prisma-premade-files/prisma-dummy.service.ts ./src/Dummy/dummy.service.ts
